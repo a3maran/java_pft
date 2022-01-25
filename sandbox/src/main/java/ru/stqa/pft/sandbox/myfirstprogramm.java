92cb1c06e8f53a289968a00a4feccd23fbd41a7e
a3maran <97895244+a3maran@users.noreply.github.com>
2022-01-25 20:43:26 +0300

    Изменена структура пакета

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myfirstprogramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myfirstprogramm.java
@@ -1,7 +1,9 @@
+package ru.stqa.pft.sandbox;
+
 public class myfirstprogramm {
 public static void main(String[] args){System.out.println("Hello, world!");}
 }
 {
 public static double distance(Point p1,Point p2)
-        
+
         }
