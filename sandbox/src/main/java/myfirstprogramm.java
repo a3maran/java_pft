ec7c9decf66c0a22036ed4c9b23c1ea929237d34
a3maran <97895244+a3maran@users.noreply.github.com>
2022-01-25 20:27:16 +0300

    Создан пакет для задания

--- sandbox/src/main/java/myfirstprogramm.java
+++ sandbox/src/main/java/myfirstprogramm.java
@@ -1,3 +1,7 @@
 public class myfirstprogramm {
 public static void main(String[] args){System.out.println("Hello, world!");}
 }
+{
+public static double distance(Point p1,Point p2)
+        
+        }
