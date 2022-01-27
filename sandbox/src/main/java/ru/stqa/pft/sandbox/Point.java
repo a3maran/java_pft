514cb441ac0dbde6989d934ae505fe0f0ed71659
a3maran <97895244+a3maran@users.noreply.github.com>
2022-01-27 14:45:14 +0300

    Задание 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,6 +1,14 @@
 package ru.stqa.pft.sandbox;
-
+//создаем класc для описания объектов=точек
 public class Point {
     public double x;
     public double y;
+
+//создаем конструктор
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 }
+
+
